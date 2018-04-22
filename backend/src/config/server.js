40f5679b530a5632a57e6e289f2e86dc8e4f2dd3
Alexandre Resende <aar6947@gmail.com>
2018-04-22 12:59:53 -0300

    chore: Allow CORS in the application

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const allowCors = require('./cors');
 
 const port = process.env.PORT || 3000;
 
@@ -9,6 +10,7 @@ const app = express();
 app
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
+  .use(allowCors)
   .listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
